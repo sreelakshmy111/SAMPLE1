792b6fb0fc555b1295797b86e724676a56e8eb00
Sreelakshmy111 <sreelakshmy009@gmail.com>
2026-02-27 16:18:16 +0530

    Add CI pipeline

--- src/components/Region.js
+++ src/components/Region.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Main from './Main'
-import './Register.css'
+import './Region.css'
 
 const Region = () => {
   const navigate = useNavigate()
